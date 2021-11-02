24250cf8e03d699367ed4be643d9142491727e79
Nikita Barinov <nikitabarinov2301@gmail.com>
2021-11-02 17:16:53 +0400

    feat: add view partial

---
+++ template/src/partial/view.ts
@@ -0,0 +1,36 @@
+import { BridgePlus } from '@happysanta/bridge-plus';
+import bridge from '@vkontakte/vk-bridge';
+import { Debug } from '../lib/debug';
+
+function fallbackInit() {
+  return new Promise((resolve) => {
+    BridgePlus.init().then(resolve);
+    window.setTimeout(resolve, 1e3);
+  });
+}
+
+export function initView() {
+  if (bridge.isWebView()) {
+    const setViewSettings = (dark = false): Promise<{ result: true }> => {
+      if (!bridge.supports('VKWebAppSetViewSettings')) return Promise.resolve({ result: true });
+
+      return bridge.send('VKWebAppSetViewSettings', {
+        status_bar_style: dark ? 'light' : 'dark',
+        action_bar_color: dark ? '#0a0a0a' : '#FFFFFF',
+      });
+    };
+
+    BridgePlus.subscribe('VKWebAppUpdateConfig', (e) => {
+      const scheme = e.scheme ?? 'client_light';
+      const isDark = scheme === 'client_dark' || scheme === 'space_gray';
+
+      const oldScheme = document.body.getAttribute('scheme');
+      if (oldScheme !== scheme) {
+        setViewSettings(isDark).catch((e) => Debug.log('setViewSettings error', e));
+        document.body.setAttribute('scheme', scheme);
+      }
+    });
+  }
+
+  return fallbackInit();
+}
